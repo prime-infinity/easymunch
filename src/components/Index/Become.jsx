be7c1c4a0b2bb7000588ec4bd087fae0c4c61cea
prime-infinity <soe.a.prime@gmail.com>
2022-05-21 21:10:16 -0700

    added illustrations

--- src/components/Index/Become.jsx
+++ src/components/Index/Become.jsx
@@ -17,15 +17,15 @@ function Become() {
             <div className="card border-0">
               <div className="card-body cur-pointer" onClick={toBeVendor}>
                 <img
-                  src="images/Become_a_vendor-compressed-min.jpg"
+                  src="illustrate/illustration (5).png"
                   alt=""
                   width=""
                   className="img-fluid rounded"
                 />
                 <a>
-                  <h5 className="card-title fw-bold pt-2">Become a Vendor</h5>
+                  <h5 className="fs-36-14 fw-bold pt-2">Become a Vendor</h5>
                 </a>
-                <p className="card-text">
+                <p className="card-text fs-14">
                   Register your restaurant with us and have your meals delivered
                   with ease.
                 </p>
@@ -36,13 +36,13 @@ function Become() {
             <div className="card border-0">
               <div className="card-body">
                 <img
-                  src="images/become_a_rider-compressed-min.jpg"
+                  src="illustrate/illustration (2).png"
                   alt=""
                   width="100%"
                   className="img-fluid rounded"
                 />
-                <h5 className="card-title fw-bold pt-2">Become a rider</h5>
-                <p className="card-text">
+                <h5 className="fs-36-14 fw-bold pt-2">Become a rider</h5>
+                <p className="card-text fs-14">
                   Join our fleet of delivery vehicles and earn good pay while
                   delivering for restaurants.
                 </p>
@@ -53,12 +53,12 @@ function Become() {
             <div className="card border-0">
               <div className="card-body">
                 <img
-                  src="images/Watch_this_space-compressed-min.jpg"
+                  src="illustrate/illustration (4).png"
                   alt=""
                   className="img-fluid rounded"
                 />
-                <h5 className="card-title fw-bold pt-2">Coming Soon!</h5>
-                <p className="card-text">
+                <h5 className="fs-36-14 fw-bold pt-2">Coming Soon!</h5>
+                <p className="card-text fs-14">
                   See latest features coming soon to our platform...
                 </p>
               </div>
@@ -67,36 +67,37 @@ function Become() {
         </div>
         {/*<!-- mobile screen -->*/}
         <div className="row d-md-none pt-5">
-          <div className="col-6">
+          <div className="col-12 cur-pointer" onClick={toBeVendor}>
+            <img
+              src="illustrate/illustration (5).png"
+              alt=""
+              width="100%"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-12">
             <a>
-              <div className="h1 fw-bold">Become a Vendor</div>
+              <div className="fs-36-14 fw-bold">Become a Vendor</div>
             </a>
-            <div className="p">
+            <div className="p fs-14">
               Register your restaurant with us and have your meals delivered
               with ease.
             </div>
           </div>
-          <div className="col-6 cur-pointer" onClick={toBeVendor}>
-            <img
-              src="images/unsplash_cQbOSRpElxw.png"
-              alt=""
-              width="100%"
-              className="img-fluid"
-            />
-          </div>
+          
         </div>
         <div className="row d-md-none pt-5">
-          <div className="col-6">
+          <div className="col-12">
             <img
-              src="images/unsplash_afDu-GuxjjMhh.png"
+              src="illustrate/illustration (2).png"
               alt=""
               width="100%"
               className="img-fluid"
             />
           </div>
-          <div className="col-6">
-            <div className="h1 fw-bold">Become a rider</div>
-            <div className="p">
+          <div className="col-12">
+            <div className="fs-36-14 fw-bold">Become a rider</div>
+            <div className="p fs-14">
               Join our fleet of delivery vehicles and earn good pay while
               delivering for restaurants.
             </div>
@@ -105,13 +106,13 @@ function Become() {
         <div className="row d-md-none pt-5">
           <div className="col-12">
             <img
-              src="images/comingsoonimg.png"
+              src="illustrate/illustration (4).png"
               width="100%"
               alt=""
               className="img-fluid"
             />
-            <div className="h1 fw-bold">Coming Soon!</div>
-            <div className="p">
+            <div className="fs-36-14 fw-bold">Coming Soon!</div>
+            <div className="p fs-14">
               See latest features coming soon to our platform...
             </div>
           </div>
